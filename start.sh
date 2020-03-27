f0e9dfb3d781d095c8445f62e80ec0e4e46a92ce
Ellen Marie Dash <me@duckie.co>
2020-03-26 22:00:13 -0400

    add neko.env.dist, and actually use neko.env.

--- start.sh
+++ start.sh
@@ -2,4 +2,15 @@
 
 cd "$(dirname "$(readlink -f $0)")"
 
+if [ ! -f neko.env ]; then
+  echo "Please run"
+  echo "    $ cp neko.env.dist neko.env"
+  echo "then edit neko.env."
+  exit 1
+fi
+
+source neko.env
+
+cat docker-compose.yaml.template | sed "s/{{VIEW_PASSWORD}}/$VIEW_PASSWORD/" | sed "s/{{ADMIN_PASSWORD}}/$ADMIN_PASSWORD/" > docker-compose.yaml
+
 docker-compose up --build
