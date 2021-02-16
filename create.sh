58463ea12a3a12c059ab1e7494c1d57572d6e33f
Ellen Marie Dash <me@duckie.co>
2021-02-15 23:57:25 -0500

    Remove dead code from create.sh.

--- create.sh
+++ create.sh
@@ -5,9 +5,6 @@ source neko.env
 
 IMAGE="docker-20-04"
 
-#COMMAND="#!/bin/bash\ngit clone https://github.com/canine-systems/neko-setup.git /root/neko-setup && echo 'NEKO_PASSWORD=\'$NEKO_PASSWORD\'' >> /root/neko-setup/neko.env && echo 'NEKO_PASSWORD_ADMIN=\'$NEKO_PASSWORD_ADMIN\'' >> /root/neko-setup/neko.env && /root/neko-setup/start.sh"
-#COMMAND="$(printf "$COMMAND" | base64)"
-#COMMAND="$(echo $COMMAND | sed 's/ /%20/g')"
 COMMAND="$(cat user-data.yml | sed "s/{{NEKO_PASSWORD}}/$NEKO_PASSWORD/" | sed "s/{{NEKO_PASSWORD_ADMIN}}/$NEKO_PASSWORD_ADMIN/")"
 
 curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $DO_TOKEN" \
